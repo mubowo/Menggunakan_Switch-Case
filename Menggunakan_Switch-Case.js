597116fb17a0afb74ab7bbb73437d28f565549e6
mubowo <mubowo@gmail.com>
2018-07-06 08:28:16 -0400

    adding Menggunakan_Switch-Case.js dan editing README.md

---
+++ Menggunakan_Switch-Case.js
@@ -0,0 +1,69 @@
+var hari = "1"; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = "5"; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = "1945"; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var bln;
+
+switch (bulan) {
+  case "1": {
+    bln = "Januari";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "2": {
+    bln = "Februari";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "3": {
+    bln = "Maret";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "4": {
+    bln = "April";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "5": {
+    bln = "Mei";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "6": {
+    bln = "Juni";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "7": {
+    bln = "Juli";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "8": {
+    bln = "Agustus";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "9": {
+    bln = "September";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "10": {
+    bln = "Oktober";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "11": {
+    bln = "November";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  case "12": {
+    bln = "Desember";
+    console.log(hari + " " + bln + " " + tahun);
+    break;
+  }
+  default:
+    console.log("Salah Input Data");
+}
